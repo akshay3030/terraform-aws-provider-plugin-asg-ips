62faed621cce001b792e3ae1d4b00b48aeee34a6
akshay3030 <akshay.dce@gmail.com>
2019-06-19 14:15:39 -0700

    update code

--- resource_asgips.go
+++ resource_asgips.go
@@ -27,10 +27,33 @@ func resourceAwsasgipsCreate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceAwsasgipsRead(d *schema.ResourceData, m interface{}) error {
+	//client := m.(*MyClient)
+
+	// Attempt to read from an upstream API
+	//obj, ok := client.Get(d.Id())
+
+	// If the resource does not exist, inform Terraform. We want to immediately
+	// return here to prevent further processing.
+	//if !ok {
+	//	d.SetId("")
+	//	return nil
+	//}
+
+	//d.Set("address", obj.Address)
 	return nil
 }
 
 func resourceAwsasgipsUpdate(d *schema.ResourceData, m interface{}) error {
+	d.Partial(true)
+
+	if d.HasChange("address") {
+		if err := updateAddress(d, m); err != nil {
+			return err
+		}
+		d.SetPartial("address")
+	}
+
+	d.Partial(false)
 	return resourceAwsasgipsRead(d, m)
 }
 
@@ -38,5 +61,9 @@ func resourceAwsasgipsDelete(d *schema.ResourceData, m interface{}) error {
 	// d.SetId("") is automatically called assuming delete returns no errors, but
 	// it is added here for explicitness.
 	d.SetId("")
+	return nil
+}
+
+func updateAddress(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
